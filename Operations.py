be2c99bad2e1a1355fe51ba854a502134aa2b095
Viv <96947328+IllusoryPig@users.noreply.github.com>
2022-01-02 13:00:19 -0500

    operations- new (#6)

--- Operations.py
+++ Operations.py
@@ -1,13 +1,26 @@
 def sub(x, y):
     return x - y if x > y else 0
 
-
 def mult(x, y):
     return x * y
 
-
 def div(x, y):
     return x / y if y != 0 else 0
+
+def pow(x, y):
+    return x ** y
+
+def rem(x, y):
+    return x % y
+
+def is_even(num):
+    return True if num % 2 == 0 else False
+
+def print_n(num):
+    print("Inside print function")
+    for x in range(1, num+ 1 ):
+        if is_even(x):
+            print(x)
 
 
 if __name__ == "__main__":
@@ -20,3 +33,12 @@ if __name__ == "__main__":
     print(div(10, 5))
 
     print(div(10, 0))
+
+    print(pow(2, 2))
+
+    print(pow(3, 4))
+
+    print(rem(5, 2))
+
+    (print_n(51))
+
