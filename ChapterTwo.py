985b2ceb53f78c976d5c78b12e838274b786dea1
Viv <96947328+IllusoryPig@users.noreply.github.com>
2022-05-29 09:53:23 -0400

    Chapter Two of the Python Crash Course book. (#8)

---
+++ ChapterTwo.py
@@ -0,0 +1,29 @@
+if __name__ == "__main__":
+    # Program 1
+    message="Ur bad and you stink like cheese"
+    print(message)
+
+    # Program 2
+    first_name = "Johnny"
+    middle_name = "and"
+    last_name = "Robbie"
+    full_name = f"{first_name} {middle_name} {last_name}"
+    message = f"What's good, {full_name.title()}?!"
+    print(message)
+
+    # Program 3
+    your_name = ' python '
+    your_name = your_name.strip()
+    print(your_name)
+
+    # Program 4
+    x=7
+    y=3
+    z=2
+    print ((x**y)+z)
+
+    # Program 5
+    # I like Minecraft.
+    print("I slayed the ender dragon :)")
+
+
